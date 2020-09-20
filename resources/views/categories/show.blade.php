e9dbe95e901467678638b2f80f9ed47b3eddb90d
haru93 <syuesuta@yahoo.co.jp>
2020-09-21 04:55:43 +0900

    カテゴリー内投稿一覧の修正

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <h2 class="bg-info text-center text-white py-3">{{ $category->name }}</h2>
-<div class="container mb-3">
-	@foreach ($articles->chunk(3) as $chunk)
+<div class="container-fluid mb-3">
+	@foreach ($articles->chunk(4) as $chunk)
 	<div class="row justify-content-center py-0">
 		@foreach ($chunk as $article)
 		<div class='text-center px-1'>
